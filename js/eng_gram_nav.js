15a5067e68d1cc2ff608fdaa52ec0ce3ad33f4d2
Por Cho <porla113@gmail.com>
2024-11-17 09:59:40 +0700

    Add grammar page and implement nav bar

---
+++ js/eng_gram_nav.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const engGrammarNav = `
+<nav>
+    <a href="../../index.html">Home</a>
+    <a href="../english.html">English</a>
+    <a href="grammar.html">Grammar</a>
+</nav>
+`;
+
+document.body.insertAdjacentHTML("afterbegin", engGrammarNav);
---
+++ js/mat_nav.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const mathNav = `
+<nav>
+    <a href="../../index.html">Home</a>
+    <a href="../math.html">Math</a>
+</nav>
+`;
+
+document.body.insertAdjacentHTML("afterbegin", mathNav);
